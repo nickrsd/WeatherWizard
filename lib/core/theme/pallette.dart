c6e07cffda88cce3e34c03155af0ca4d3754d60b
Dominic Rodriquez <nickrsd@gmail.com>
2024-03-31 09:23:58 -0700

    Color is king
    Black background to blend with the initial and loading states.

--- lib/core/theme/pallette.dart
+++ lib/core/theme/pallette.dart
@@ -1,7 +1,7 @@
 import "package:flutter/material.dart";
 
 class AppPallete {
-  static const Color backgroundColor = Color.fromRGBO(24, 24, 32, 1);
+  static const Color backgroundColor = Color.fromRGBO(0, 0, 0, 1);
   static const Color gradient1 = Color.fromRGBO(187, 63, 221, 1);
   static const Color gradient2 = Color.fromRGBO(251, 109, 169, 1);
   static const Color gradient3 = Color.fromRGBO(255, 159, 124, 1);
